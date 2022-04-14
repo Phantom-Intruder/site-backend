44a7063a4f86c04a4bb5273c396d9ff371e8f71f
Mewantha <me@mewantha.net>
2022-04-14 12:34:33 +0530

    New block commited.

--- controllers/commit.js
+++ controllers/commit.js
@@ -6,14 +6,36 @@ exports.postCommit = (req, res, next) => {
     const commitText = req.body.commitText;
     //Commit(commitText).
     var execProcess = require("./exec_process.js");
-    execProcess.result("git clone https://github.com/Phantom-Intruder/site.git", function(err, response){
-        if(!err){
-            console.log(response);
-            var logStream = fs.createWriteStream('../site/src/app/app.component.html', {flags: 'a'});
-            logStream.write('<p>This is a commit block</p>');
-        }else {
-            console.log(err);
+    execProcess.result("git clone https://github.com/Phantom-Intruder/site.git & git checkout -b topic/work-on-site", (err, response) => {
+        if (err){
+            execProcess.result("cd ../site & git checkout -b topic/work-on-site & git pull origin topic/work-on-site", (err, response) => {
+                if(!err){
+                    console.log(response);
+                    //console.log(response);
+                    var logStream = fs.createWriteStream('site/src/app/app.component.html', {flags: 'a'});
+                    logStream.write('<br/><p>This is a commit block</p>');
+                    execProcess.result('git add . & git commit -m "New block commited." & git push -u origin topic/work-on-site', (err, response) => {
+                        if(!err){
+                            console.log(response);
+                        } else {
+                            console.log(err);
+                        }
+                    });
+                }else {
+                    console.log(err);
+                    console.log("There was an err in method")
+                    return err
+                    //console.log(err);
+                }
+            })
         }
+    })
+    .then(value => {
+        console.log("asdadasdasdasd");
+    })
+    .catch(err => {
+        console.log("There was an err in promise")
+        console.log(err);
     })
 }
 
